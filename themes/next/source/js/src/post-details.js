e80f44164025d57a60435de1b167b2838b7c5381
yangjiajun <yangjiajun@boohee.com>
2018-01-12 16:26:07 +0800

    upgrade theme version

--- themes/next/source/js/src/post-details.js
+++ themes/next/source/js/src/post-details.js
@@ -3,10 +3,8 @@
 $(document).ready(function () {
 
   initScrollSpy();
-  NexT.utils.needAffix() && initAffix();
-  initTOCDimension();
 
-  function initScrollSpy() {
+  function initScrollSpy () {
     var tocSelector = '.post-toc';
     var $tocElement = $(tocSelector);
     var activeCurrentSelector = '.active-current';
@@ -17,60 +15,18 @@ $(document).ready(function () {
 
         removeCurrentActiveClass();
         $currentActiveElement.addClass('active-current');
+
+        // Scrolling to center active TOC element if TOC content is taller then viewport.
+        $tocElement.scrollTop($currentActiveElement.offset().top - $tocElement.offset().top + $tocElement.scrollTop() - ($tocElement.height() / 2));
       })
       .on('clear.bs.scrollspy', removeCurrentActiveClass);
 
-    $('body').scrollspy({target: tocSelector});
+    $('body').scrollspy({ target: tocSelector });
 
-    function removeCurrentActiveClass() {
+    function removeCurrentActiveClass () {
       $(tocSelector + ' ' + activeCurrentSelector)
         .removeClass(activeCurrentSelector.substring(1));
     }
-  }
-
-  // Sidebar float
-  function initAffix() {
-    var headerHeight = $('.header-inner').height();
-    var footerOffset = parseInt($('.main').css('padding-bottom'), 10);
-    var sidebarTop = (CONFIG.sidebar.offset_float === 0) ? headerHeight + CONFIG.sidebar.offset : headerHeight;
-
-    $('.sidebar-inner').affix({
-      offset: {
-        top: sidebarTop,
-        bottom: footerOffset
-      }
-    });
-
-    $(document)
-      .on('affixed.bs.affix', function () {
-        updateTOCHeight(document.body.clientHeight - 100);
-      });
-  }
-
-  function initTOCDimension() {
-    var updateTOCHeightTimer;
-
-    $(window).on('resize', function () {
-      updateTOCHeightTimer && clearTimeout(updateTOCHeightTimer);
-
-      updateTOCHeightTimer = setTimeout(function () {
-        var tocWrapperHeight = document.body.clientHeight - 100;
-
-        updateTOCHeight(tocWrapperHeight);
-      }, 0);
-    });
-
-    // Initialize TOC Height.
-    updateTOCHeight(document.body.clientHeight - 100);
-
-    // Initialize TOC Width.
-    var scrollbarWidth = NexT.utils.getScrollbarWidth();
-    $('.post-toc').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
-  }
-
-  function updateTOCHeight(height) {
-    height = height || 'auto';
-    $('.post-toc').css('max-height', height);
   }
 
 });
@@ -98,12 +54,12 @@ $(document).ready(function () {
           .velocity('transition.slideDownIn', TAB_ANIMATE_DURATION)
           .addClass(activePanelClassName);
       }) :
-      currentTarget.animate({opacity: 0}, TAB_ANIMATE_DURATION, function () {
+      currentTarget.animate({ opacity: 0 }, TAB_ANIMATE_DURATION, function () {
         currentTarget.hide();
         target
           .stop()
           .css({'opacity': 0, 'display': 'block'})
-          .animate({opacity: 1}, TAB_ANIMATE_DURATION, function () {
+          .animate({ opacity: 1 }, TAB_ANIMATE_DURATION, function () {
             currentTarget.removeClass(activePanelClassName);
             target.addClass(activePanelClassName);
           });
@@ -113,6 +69,7 @@ $(document).ready(function () {
     item.addClass(activeTabClassName);
   });
 
+  // TOC item animation navigate & prevent #item selector in adress bar.
   $('.post-toc a').on('click', function (e) {
     e.preventDefault();
     var targetSelector = NexT.utils.escapeSelector(this.getAttribute('href'));
@@ -120,7 +77,7 @@ $(document).ready(function () {
 
     hasVelocity ?
       html.velocity('stop').velocity('scroll', {
-        offset: offset + 'px',
+        offset: offset  + 'px',
         mobileHA: false
       }) :
       $('html, body').stop().animate({
@@ -129,13 +86,14 @@ $(document).ready(function () {
   });
 
   // Expand sidebar on post detail page by default, when post has a toc.
-  NexT.motion.middleWares.sidebar = function () {
-    var $tocContent = $('.post-toc-content');
-
-    if (CONFIG.sidebar.display === 'post' || CONFIG.sidebar.display === 'always') {
-      if ($tocContent.length > 0 && $tocContent.html().trim().length > 0) {
-        NexT.utils.displaySidebar();
-      }
-    }
-  };
+  var $tocContent = $('.post-toc-content');
+  var isSidebarCouldDisplay = CONFIG.sidebar.display === 'post' ||
+      CONFIG.sidebar.display === 'always';
+  var hasTOC = $tocContent.length > 0 && $tocContent.html().trim().length > 0;
+  if (isSidebarCouldDisplay && hasTOC) {
+    CONFIG.motion.enable ?
+      (NexT.motion.middleWares.sidebar = function () {
+          NexT.utils.displaySidebar();
+      }) : NexT.utils.displaySidebar();
+  }
 });
--- themes/next/source/js/src/scrollspy.js
+++ themes/next/source/js/src/scrollspy.js
@@ -1,10 +1,10 @@
 /* ========================================================================
- * Bootstrap: scrollspy.js v3.3.2
- * http://getbootstrap.com/javascript/#scrollspy
- * ========================================================================
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
+* Bootstrap: scrollspy.js v3.3.2
+* http://getbootstrap.com/javascript/#scrollspy
+* ========================================================================
+* Copyright 2011-2015 Twitter, Inc.
+* Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+* ======================================================================== */
 
 /**
  * Custom by iissnan
@@ -21,75 +21,73 @@
   // ==========================
 
   function ScrollSpy(element, options) {
-    this.$body = $(document.body);
-    this.$scrollElement = $(element).is(document.body) ? $(window) : $(element);
-    this.options = $.extend({}, ScrollSpy.DEFAULTS, options);
-    this.selector = (this.options.target || '') + ' .nav li > a';
-    this.offsets = [];
-    this.targets = [];
-    this.activeTarget = null;
-    this.scrollHeight = 0;
+    this.$body          = $(document.body)
+    this.$scrollElement = $(element).is(document.body) ? $(window) : $(element)
+    this.options        = $.extend({}, ScrollSpy.DEFAULTS, options)
+    this.selector       = (this.options.target || '') + ' .nav li > a'
+    this.offsets        = []
+    this.targets        = []
+    this.activeTarget   = null
+    this.scrollHeight   = 0
 
-    this.$scrollElement.on('scroll.bs.scrollspy', $.proxy(this.process, this));
-    this.refresh();
+    this.$scrollElement.on('scroll.bs.scrollspy', $.proxy(this.process, this))
+    this.refresh()
     this.process()
   }
 
-  ScrollSpy.VERSION = '3.3.2';
+  ScrollSpy.VERSION  = '3.3.2'
 
   ScrollSpy.DEFAULTS = {
     offset: 10
-  };
+  }
 
   ScrollSpy.prototype.getScrollHeight = function () {
     return this.$scrollElement[0].scrollHeight || Math.max(this.$body[0].scrollHeight, document.documentElement.scrollHeight)
-  };
+  }
 
   ScrollSpy.prototype.refresh = function () {
-    var that = this;
-    var offsetMethod = 'offset';
-    var offsetBase = 0;
+    var that          = this
+    var offsetMethod  = 'offset'
+    var offsetBase    = 0
 
-    this.offsets = [];
-    this.targets = [];
-    this.scrollHeight = this.getScrollHeight();
+    this.offsets      = []
+    this.targets      = []
+    this.scrollHeight = this.getScrollHeight()
 
     if (!$.isWindow(this.$scrollElement[0])) {
-      offsetMethod = 'position';
-      offsetBase = this.$scrollElement.scrollTop()
+      offsetMethod = 'position'
+      offsetBase   = this.$scrollElement.scrollTop()
     }
 
     this.$body
       .find(this.selector)
       .map(function () {
-        var $el = $(this);
-        var href = $el.data('target') || $el.attr('href');
-        var $href = /^#./.test(href) && $(NexT.utils.escapeSelector(href)); // Need to escape selector.
+        var $el   = $(this)
+        var href  = $el.data('target') || $el.attr('href')
+        var $href = /^#./.test(href) && $(NexT.utils.escapeSelector(href)) // Need to escape selector.
 
         return ($href
           && $href.length
           && $href.is(':visible')
           && [[$href[offsetMethod]().top + offsetBase, href]]) || null
       })
-      .sort(function (a, b) {
-        return a[0] - b[0]
-      })
+      .sort(function (a, b) { return a[0] - b[0] })
       .each(function () {
-        that.offsets.push(this[0]);
+        that.offsets.push(this[0])
         that.targets.push(this[1])
       })
 
 
-  };
+  }
 
   ScrollSpy.prototype.process = function () {
-    var scrollTop = this.$scrollElement.scrollTop() + this.options.offset;
-    var scrollHeight = this.getScrollHeight();
-    var maxScroll = this.options.offset + scrollHeight - this.$scrollElement.height();
-    var offsets = this.offsets;
-    var targets = this.targets;
-    var activeTarget = this.activeTarget;
-    var i;
+    var scrollTop    = this.$scrollElement.scrollTop() + this.options.offset
+    var scrollHeight = this.getScrollHeight()
+    var maxScroll    = this.options.offset + scrollHeight - this.$scrollElement.height()
+    var offsets      = this.offsets
+    var targets      = this.targets
+    var activeTarget = this.activeTarget
+    var i
 
     if (this.scrollHeight != scrollHeight) {
       this.refresh()
@@ -100,31 +98,31 @@
     }
 
     if (activeTarget && scrollTop < offsets[0]) {
-      $(this.selector).trigger('clear.bs.scrollspy');  // Add a custom event.
-      this.activeTarget = null;
+      $(this.selector).trigger('clear.bs.scrollspy')  // Add a custom event.
+      this.activeTarget = null
       return this.clear()
     }
 
     for (i = offsets.length; i--;) {
       activeTarget != targets[i]
-      && scrollTop >= offsets[i]
-      && (!offsets[i + 1] || scrollTop <= offsets[i + 1])
-      && this.activate(targets[i])
+        && scrollTop >= offsets[i]
+        && (!offsets[i + 1] || scrollTop <= offsets[i + 1])
+        && this.activate(targets[i])
     }
-  };
+  }
 
   ScrollSpy.prototype.activate = function (target) {
-    this.activeTarget = target;
+    this.activeTarget = target
 
-    this.clear();
+    this.clear()
 
     var selector = this.selector +
       '[data-target="' + target + '"],' +
-      this.selector + '[href="' + target + '"]';
+      this.selector + '[href="' + target + '"]'
 
     var active = $(selector)
       .parents('li')
-      .addClass('active');
+      .addClass('active')
 
     if (active.parent('.dropdown-menu').length) {
       active = active
@@ -133,13 +131,13 @@
     }
 
     active.trigger('activate.bs.scrollspy')
-  };
+  }
 
   ScrollSpy.prototype.clear = function () {
     $(this.selector)
       .parentsUntil(this.options.target, '.active')
       .removeClass('active')
-  };
+  }
 
 
   // SCROLLSPY PLUGIN DEFINITION
@@ -147,28 +145,28 @@
 
   function Plugin(option) {
     return this.each(function () {
-      var $this = $(this);
-      var data = $this.data('bs.scrollspy');
-      var options = typeof option == 'object' && option;
+      var $this   = $(this)
+      var data    = $this.data('bs.scrollspy')
+      var options = typeof option == 'object' && option
 
-      if (!data) $this.data('bs.scrollspy', (data = new ScrollSpy(this, options)));
+      if (!data) $this.data('bs.scrollspy', (data = new ScrollSpy(this, options)))
       if (typeof option == 'string') data[option]()
     })
   }
 
-  var old = $.fn.scrollspy;
+  var old = $.fn.scrollspy
 
-  $.fn.scrollspy = Plugin;
-  $.fn.scrollspy.Constructor = ScrollSpy;
+  $.fn.scrollspy             = Plugin
+  $.fn.scrollspy.Constructor = ScrollSpy
 
 
   // SCROLLSPY NO CONFLICT
   // =====================
 
   $.fn.scrollspy.noConflict = function () {
-    $.fn.scrollspy = old;
+    $.fn.scrollspy = old
     return this
-  };
+  }
 
 
   // SCROLLSPY DATA-API
@@ -176,7 +174,7 @@
 
   $(window).on('load.bs.scrollspy.data-api', function () {
     $('[data-spy="scroll"]').each(function () {
-      var $spy = $(this);
+      var $spy = $(this)
       Plugin.call($spy, $spy.data())
     })
   })
